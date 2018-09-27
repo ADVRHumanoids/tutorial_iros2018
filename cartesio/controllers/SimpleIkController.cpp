f8eb8ad397f9452366128de633d01d91d675ade6
Arturo Laurenzi <arturo.laurenzi@iit.it>
2018-09-27 11:47:58 +0200

    Added cartesio controller wrapper

---
+++ cartesio/controllers/SimpleIkController.cpp
@@ -0,0 +1,77 @@
+#include <SimpleIkController.h>
+
+
+XBot::Cartesian::SimpleIk::SimpleIk(XBot::ModelInterface::Ptr model): 
+    CartesianInterfaceImpl(model, get_task_list(*model))
+{
+    _opensot = boost::make_shared<OpenSoT::IKProblem>(*model, 0.01);
+}
+
+bool XBot::Cartesian::SimpleIk::update(double time, double period)
+{
+    XBot::Cartesian::CartesianInterfaceImpl::update(time, period);
+    
+    _model->getJointPosition(_q);
+    
+    Eigen::Affine3d Tref;
+    if(getPoseReference(_opensot->_arm->getDistalLink(), Tref))
+    {
+        _opensot->_arm->setReference(Tref.matrix());
+    }
+    
+    if(getReferencePosture(_posture_ref))
+    {
+        _opensot->_posture->setReference(_posture_ref);
+    }
+    
+    _opensot->update(_q);
+    if(!_opensot->solve(_dq))
+    {
+        return false;
+    }
+    
+    _model->setJointVelocity(_dq/period);
+    
+    return true;
+}
+
+bool XBot::Cartesian::SimpleIk::setBaseLink(const std::string& ee_name, const std::string& new_base_link)
+{
+    if(!XBot::Cartesian::CartesianInterfaceImpl::setBaseLink(ee_name, new_base_link))
+    {
+        return false;
+    }
+    
+    if(_opensot->_arm->getDistalLink() == ee_name && _opensot->_arm->setBaseLink(new_base_link))
+    {
+        return true;
+    }
+    
+    XBot::Logger::error("SimpleIk::setBaseLink unable to change base link \n");
+    return false;
+}
+
+XBot::Cartesian::ProblemDescription XBot::Cartesian::SimpleIk::get_task_list(const ModelInterface& model)
+{
+    auto arm_cartesian = MakeCartesian(model.chain("left_arm").getTipLinkName(),
+                                       model.chain("torso").getBaseLinkName());
+    
+    auto postural = MakePostural(model.getJointNum());
+    
+    XBot::Cartesian::ProblemDescription desc(arm_cartesian + postural);
+    
+    return desc;
+}
+
+
+extern "C" XBot::Cartesian::CartesianInterface* create_instance(XBot::ModelInterface::Ptr model,
+                                                                XBot::Cartesian::ProblemDescription pb)
+{
+    return new XBot::Cartesian::SimpleIk(model);
+}
+
+extern "C" void destroy_instance( XBot::Cartesian::CartesianInterface* instance )
+{
+    delete instance;
+}
+
---
+++ cartesio/controllers/SimpleIkController.h
@@ -0,0 +1,32 @@
+#include <cartesian_interface/CartesianInterfaceImpl.h>
+#include <IKProblem.h>
+
+namespace XBot { namespace Cartesian {
+   
+    class SimpleIk : public CartesianInterfaceImpl {
+        
+    public:
+        
+        SimpleIk(ModelInterface::Ptr model);
+        
+        virtual bool update(double time, double period);
+        
+        virtual bool setBaseLink(const std::string& ee_name, 
+                                 const std::string& new_base_link);
+        
+        
+        
+    private:
+        
+        static ProblemDescription get_task_list(const ModelInterface& model);
+        
+        OpenSoT::IKProblem::Ptr _opensot;
+        
+        Eigen::VectorXd _posture_ref, _dq, _q;
+        
+    };
+    
+    
+} }
+
+
