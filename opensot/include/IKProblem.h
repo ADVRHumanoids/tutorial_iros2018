2f89dfb19121a2de5e76d7145d9146e1e275e742
EnricoMingo <enricomingo@gmail.com>
2018-09-30 19:05:28 +0200

    added some documentation

--- opensot/include/IKProblem.h
+++ opensot/include/IKProblem.h
@@ -9,16 +9,40 @@
 
 namespace OpenSoT{
 
+/**
+ * @brief The IKProblem class wraps the tasks, constraints and the solver used to solve the IK
+ */
 class IKProblem{
 
 public:
     typedef boost::shared_ptr<IKProblem> Ptr;
 
+    /**
+     * @brief IKProblem constructor
+     * @param model pointer to external model
+     * @param dT control loop
+     */
     IKProblem(XBot::ModelInterface::Ptr model, const double dT);
     ~IKProblem();
 
+    /**
+     * @brief solve call this after update()
+     * @param x solution form solver (dq)
+     * @return true if solved
+     */
     bool solve(Eigen::VectorXd& x);
+
+    /**
+     * @brief update call after the model.update() to update the autostack
+     * @param x input state q
+     * @param use_inertia_matrix if true the postural task is weighted with the inertia matrix
+     */
     void update(Eigen::VectorXd& x, bool use_inertia_matrix = false);
+
+    /**
+     * @brief log to log solver and autostaack status
+     * @param logger a pointer to a MatLogger
+     */
     void log(XBot::MatLogger::Ptr& logger);
 
     tasks::velocity::Cartesian::Ptr _left_arm, _right_arm;
